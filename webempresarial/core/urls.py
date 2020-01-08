5d4aafed7aea1a580c82adfcd2102888aa983522
CamiloGato <cm.andres02@hotmail.com>
2020-01-08 16:15:07 -0500

    Removed

--- webempresarial/core/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('store/', views.store, name='store'),
-    path('about/', views.about, name='about'),
-    path('', views.home, name='home'),
-]
